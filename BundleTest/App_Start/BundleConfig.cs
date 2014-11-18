dc64799245b7d73d84c61918bf5919ebbb80c16a
Max Brodin <max.brodin@developex.com>
2014-11-18 13:01:04 +0200

    Initial commit

---
+++ BundleTest/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace BundleTest
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();            
+            
+            bundles.Add(new ScriptBundle("~/bundles/library")
+                .IncludeDirectory("~/Scripts/Libraries", "*.js", true));
+        }
+    }
+}
